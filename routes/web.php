8e7e331b33899f0b544bf3a03476161167103887
合田一心 <@gmail.com>
2022-03-24 02:17:35 +0000

    タスクの詳細閲覧を書いた

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,13 @@ Route::get('/', [AuthController::class, 'index'])->name('front.index');
 Route::post('/login', [AuthController::class, 'login']);
 // 認可処理
 Route::middleware(['auth'])->group(function () {
-    Route::get('/task/list', [TaskController::class, 'list']);
-    Route::post('/task/register', [TaskController::class, 'register']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::prefix('/task')->group(function () {
+        Route::get('/task/list', [TaskController::class, 'list']);
+        Route::post('/task/register', [TaskController::class, 'register']);
+        Route::get('/task/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
+        });
+        //
+        Route::get('/logout', [AuthController::class, 'logout']);
 });
 
 //テスト用
